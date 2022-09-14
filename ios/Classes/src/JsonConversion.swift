ea399beefcdecf6ef282bc62189dded2374462e5
choim <choim4389@gmail.com>
2022-09-14 13:25:29 +0900

    correct type

--- ios/Classes/src/JsonConversion.swift
+++ ios/Classes/src/JsonConversion.swift
@@ -83,7 +83,7 @@ public func toOverlayImage(assetName: String, registrar: FlutterPluginRegistrar)
 }
 
 public func toOverlayImageFromBitmap(bitmap: [UInt8]) -> NMFOverlayImage? {
-    let uiImage = UIImage(NSData(bytes: bitmap, length: bitmap.count))
+    let uiImage = UIImage(Data.init(bytes: bitmap))
     return NMFOverlayImage(image: uiImage)
 }
 
